11859c27690c61c05848cde1f25cc96e75ac63fc
David Vargas <dvargas92495@gmail.com>
2023-05-10 20:53:38 -0400

    ready for fire query context

--- api/message.ts
+++ api/message.ts
@@ -1,33 +1,8 @@
 import createBackendClientHandler from "samepage/backend/createBackendClientHandler";
+import { notebookRequestNodeQuerySchema } from "samepage/internal/types";
 import decodeState from "src/util/decodeState";
 import encodeState from "src/util/encodeState";
-// import { notebookRequestNodeQuerySchema } from "samepage/internal/types";
-import { z } from "zod";
-const zCondition = z.object({
-  source: z.string(),
-  target: z.string(),
-  relation: z.string(),
-});
-
-const zSelection = z.object({
-  label: z.string(),
-  text: z.string(),
-});
-
-export const notebookRequestNodeQuerySchema = z.object({
-  schema: z.literal("node-query"),
-  conditions: zCondition.array().optional().default([]),
-  returnNode: z.string(),
-  selections: zSelection.array().optional().default([]),
-});
-
-const fireNodeQuery = (
-  args: z.infer<typeof notebookRequestNodeQuerySchema>,
-  token: string
-) => {
-  // TODO
-  return [];
-};
+import fireNodeQuery from "src/util/fireNodeQuery";
 
 const message = (args: Record<string, unknown>) => {
   return createBackendClientHandler({
@@ -41,7 +16,7 @@ const message = (args: Record<string, unknown>) => {
         if (request.schema === "node-query") {
           const result = notebookRequestNodeQuerySchema.safeParse(request);
           if (!result.success) return {};
-          const results = await fireNodeQuery(result.data, token);
+          const results = await fireNodeQuery(result.data, {});
           return {
             results,
           };
---
+++ src/util/fireNodeQuery.ts
@@ -0,0 +1,93 @@
+// This algorithm is heavily inspired by datalog
+import type { notebookRequestNodeQuerySchema } from "samepage/internal/types";
+import { z } from "zod";
+import crypto from "crypto";
+
+type NodeAssignment = { id: string };
+type AssignmentValue = NodeAssignment | string | number | RegExp;
+type Assignment = Record<string, AssignmentValue>;
+const isNode = (v: Assignment[string]): v is NodeAssignment =>
+  typeof v === "object" && v !== null && !(v instanceof RegExp);
+type Program = {
+  assignments: Record<string, Assignment>;
+  vars: Set<string>;
+};
+
+const hashAssignment = (assignment: Assignment) => {
+  const hash = crypto.createHash("sha256");
+  Object.keys(assignment)
+    .sort()
+    .forEach((key) => {
+      const raw = assignment[key];
+      const value = isNode(raw)
+        ? raw.id
+        : raw instanceof RegExp
+        ? raw.source
+        : typeof raw === "number"
+        ? raw.toString()
+        : raw;
+      hash.update(key).update(value);
+    });
+  return hash.digest("hex");
+};
+
+const fireNodeQuery = async (
+  args: z.infer<typeof notebookRequestNodeQuerySchema>,
+  context: Record<
+    string,
+    (args: {
+      program: Program;
+      source: string;
+      target: string;
+    }) => Promise<Program>
+  >
+) => {
+  const getAssignments = async (
+    conditions: z.infer<typeof notebookRequestNodeQuerySchema>["conditions"],
+    initialVars: string[] = []
+  ) => {
+    const { assignments } = await conditions.reduce(
+      (prev, condition, index) =>
+        prev.then(async (program) => {
+          if (Object.keys(program.assignments).length === 0 && index > 0)
+            return program;
+          const join = (matches: Assignment[], vars: string[]) => {
+            vars.forEach((v) => program.vars.add(v));
+            matches.forEach((match) => {
+              const hash = hashAssignment(match);
+              if (program.assignments[hash]) return;
+              program.assignments[hash] = match;
+            });
+          };
+          const handler = context[condition.relation];
+          if (!handler) return program;
+          return handler({
+            program,
+            source: condition.source,
+            target: condition.target,
+          });
+        }),
+      Promise.resolve<Program>({
+        assignments: {},
+        vars: new Set<string>(initialVars),
+      })
+    );
+    return assignments;
+  };
+  const assignments = getAssignments(args.conditions);
+
+  const results = Object.values(assignments).map((res) => {
+    const returnNodeValue = res[args.returnNode];
+    const returnNode = isNode(returnNodeValue)
+      ? returnNodeValue
+      : typeof returnNodeValue !== "object"
+      ? { id: returnNodeValue.toString() }
+      : { id: returnNodeValue.source };
+    return args.selections.reduce((result, selection) => {
+      return result;
+    }, returnNode);
+  });
+  return results;
+};
+
+export default fireNodeQuery;
